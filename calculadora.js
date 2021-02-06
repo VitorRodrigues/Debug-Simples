993029aa6607f031e07e6859709d0d9fcae1e63b
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2021-02-06 18:15:17 +0000

    Commit inicial da calculadora e do formulario

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+
+var Calculadora = {
+    // Calcula a soma de dois números
+    soma: function (num1, num2) {
+
+        let x = num1
+        let y = num2
+        let soma = x + y
+
+        return soma
+    }
+}
